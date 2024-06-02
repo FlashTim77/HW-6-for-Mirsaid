45ca3554877873180d8149011655a6ff0e9413e6
Timur Tursunov <t.timon2003@gmail.com>
2024-06-02 20:43:02 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,112 @@
+#1
+# a = int(input("Напишите ваше число: "))
+# a = a + a * 2
+# print(a)
+
+
+#2
+# a = 46
+# b = str(a * 5)
+# print(a, b)
+
+#3
+# a = (5, 'F', 'Привет', 90.2)
+# b = {67}
+# print(a[2])
+
+#4
+# a = input("Напишите 4-х значное число: ")
+# for i in a:
+#     print(i)
+
+
+#5
+# a = input("Your name: ")
+# b = input("Your surname: ")
+# c = input("Your age: ")
+# print(a)
+# print(b)
+# print(c)
+
+#6
+# a = int(input("first num: "))
+# b = int(input("second num: "))
+# c = int(input("third num: "))
+# i = (a + b - c)
+# print(i)
+
+#7
+# a = int(3)
+# b = int(2.4)
+# c = int('6')
+# i = (a * b * c)
+# print(i)
+
+
+#8
+
+# a = int(input("First num: "))
+# b = int(input("Second num: "))
+# c = int(input("Third num: "))
+# i = a + b + c
+# print(i)
+
+#9
+
+# a = input("Write your password: ")
+# b = input("Confirm your password: ")
+# if a == b:
+#     print("Good! ")
+# else:
+#     print("Error, try again")
+
+
+#10
+# a = int(input("Write first number: "))
+# b = int(input("Write second number: "))
+# c = input("Choose math operation: (+, -, *, /): ")
+# try:
+#     if c == '+':
+#         print(a + b)
+#     elif c == '-':
+#         print(a - b)
+#     elif c == '*':
+#         print(a * b)
+#     elif c == '/':
+#         print(a / b)
+# except:
+#     print("на 0 делить нельзя")
+
+
+#11
+# print("Hello, World!")
+
+#12
+#  Переменная это кейс который может хранить в себе данные
+# для работы с помощью данными можно вызвать переменную
+
+#13
+# a = int(input("your num: "))
+# if a / 2:
+#     print("чет")
+# else:
+#     print("нечет")
+
+#14
+#цикл - когда программа повторяет одно и тоже действие несколько раз
+#for, while - так же чтобы остановить цикл нужно использовать break
+
+#15
+
+#список: list(), list[]
+# также с помощью массива можно вызывать по индексу
+
+#16
+#функция один из основных инструментов для создания удобного кода
+
+#17
+# механизм который дает возможность контролировать ошибки программисту в коде try: except:
+
+#18
+
+#открыть проограмму для кодирования vs code или pycharm, создать файл main.py
